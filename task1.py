51495ab081d20cde2cb058ce05769d69ad871bcb
kostenkoSergei <sdh1985@mail.ru>
2020-05-22 09:16:37 +0300

    improved task1

--- task1.py
+++ task1.py
@@ -1,11 +1,14 @@
 from sys import argv
 
-script_name, output, rate, premium = argv
+try:
+    script_name, output, rate, premium = argv
 
 
-def salary_calc(x, y, z):
-    result = int(x) * int(y) + int(z)
-    return f'Заработная плата составила {result}'
+    def salary_calc(x, y, z):
+        result = int(x) * int(y) + int(z)
+        return f'Заработная плата составила {result}'
 
 
-print(f'При выработке {output} часов, ставке {rate} в час и премии {premium} {salary_calc(output, rate, premium)}')
+    print(f'При выработке {output} часов, ставке {rate} в час и премии {premium} {salary_calc(output, rate, premium)}')
+except ValueError as v:
+    print(v)
