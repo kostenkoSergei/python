b6c1c4018aedb98bccd30291972b804ca9a66f10
kostenkoSergei <sdh1985@mail.ru>
2020-06-04 00:02:45 +0300

    created task4

--- task3.py
+++ task3.py
@@ -1,22 +1,29 @@
-import traceback
-
-
 class OwnError(ZeroDivisionError):
     def __init__(self, warning_text):
         """Creates an warning message if exception was happened"""
         self.txt = warning_text
 
 
-user_list = []
-while True:
-    user_input = input('Enter some number or str. If you want to finish enter \'stop\': ')
-    if user_input == 'stop':
-        print(user_list)
-        break
-    try:
-        if not user_input.isdigit():
-            raise OwnError('You have to enter only numbers')
-    except OwnError as error:
-        print(error)
-    else:
-        user_list.append(float(user_input))
+class MyClass:
+    def __init__(self):
+        self.user_list = []
+
+    def __call__(self, *args, **kwargs):
+        """Return list that consists only from numbers"""
+        while True:
+            user_input = input('Enter some number or str. If you want to finish enter \'stop\': ').strip()
+            if user_input == 'stop':
+                return self.user_list
+                break
+            try:
+                if not user_input.isdigit():
+                    raise OwnError('You have to enter only integer numbers')
+            except OwnError as error:
+                print(error)
+            else:
+                self.user_list.append(int(user_input))
+
+
+some_obj = MyClass()
+some_obj()
+print(getattr(some_obj, 'user_list'))
